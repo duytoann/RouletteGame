962194a1f7138d8d1ed465cd852312411bd4ed1e
uyit14 <uytaibao@gmail.com>
2019-12-16 09:39:36 +0700

    update function

--- app/src/main/java/com/roulettegame/roulettegameapp/MainActivity.kt
+++ app/src/main/java/com/roulettegame/roulettegameapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.roulettegame.roulettegameapp
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.View
 import android.view.animation.Animation
@@ -25,6 +26,7 @@ class MainActivity : AppCompatActivity() {
     private var mDegree : Float = 0f
     private var mDegreeOld : Float = 0f
     private var tempDegree : Float = 0f
+    private var initDegree : Float = 18f
     private var valueOfSum: String = ""
     //private val caseOfGift: IntArray = intArrayOf(0, 0, 1, 4, 0, 0, 0, 0, 3, 3, 0, 3, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 5, 0, 0, 4, 0, 5, 0, 0, 3, 0, 4, 0, 3, 5, 0, 0, 0, 0, 0, 0, 1, 1, 0, 6, 2, 0, 2, 0, 0, 0, 1, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 4, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 3, 0, 0, 4, 0, 0, 0, 0, 4, 1, 1, 0, 0, 0, 0, 1, 0, 0, 2, 0, 0, 6, 4, 1, 0, 0, 0, 4, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 5, 3, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 5, 0)
     private var caseOfGift = ""
@@ -64,7 +66,7 @@ class MainActivity : AppCompatActivity() {
                 '5' -> tempDegree = 126f
                 '6' -> tempDegree = 54f
             }
-            mDegree = (360-tempDegree + 2160)
+            mDegree = (360-(tempDegree-initDegree) + 2160)
             val mRotate = RotateAnimation(
                 mDegreeOld, mDegree,
                 RotateAnimation.RELATIVE_TO_SELF, 0.5f,
@@ -82,7 +84,7 @@ class MainActivity : AppCompatActivity() {
                     //stop
                     gimWaiting.visibility = View.GONE
                     imGift.visibility = View.VISIBLE
-                    tvGiftName.text = getGift(360- mDegree % 360)
+                    tvGiftName.text = getGift2(360- mDegree % 360)
                     val editor = sharedPref.edit()
                     val index = sharedPref.getInt(PREF_INDEX, 0) + 1
                     editor.putInt(PREF_INDEX, index)
@@ -96,24 +98,56 @@ class MainActivity : AppCompatActivity() {
             imRoulette.startAnimation(mRotate)
         }
 
+//        //clear pref
+//        imPointer.setOnLongClickListener{
+//            val editor = sharedPref.edit()
+//            editor.putInt(PREF_INDEX, 0)
+//            editor.apply()
+//            Toast.makeText(this, sharedPref.getInt(PREF_INDEX, -1).toString(), Toast.LENGTH_SHORT).show()
+//            true
+//        }
         //clear pref
-        imPointer.setOnLongClickListener{
-            val editor = sharedPref.edit()
-            editor.putInt(PREF_INDEX, 0)
-            editor.apply()
-            Toast.makeText(this, sharedPref.getInt(PREF_INDEX, -1).toString(), Toast.LENGTH_SHORT).show()
-            true
+        var count = 0
+        imPointer.setOnClickListener {
+            count++
+            Handler().postDelayed({
+                if(count < 5){
+                    count=0
+                }
+            }, 1000)
+            if(count==5){
+                val editor = sharedPref.edit()
+                editor.putInt(PREF_INDEX, 0)
+                editor.apply()
+                Toast.makeText(this, sharedPref.getInt(PREF_INDEX, -1).toString(), Toast.LENGTH_SHORT).show()
+                count = 0
+            }
         }
 
-        //show/hidden edit text
-        btn_change_value.setOnLongClickListener{
-            val intent = Intent(this, AdminActivity::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_up)
-            true
+
+//        //show/hidden adminpage
+//        btn_change_value.setOnLongClickListener{
+//            val intent = Intent(this, AdminActivity::class.java)
+//            startActivity(intent)
+//            overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_up)
+//            true
+//        }
+        //
+        var count2 = 0
+        btn_change_value.setOnClickListener {
+            count2++
+            Handler().postDelayed({
+                if(count2 < 5){
+                    count2=0
+                }
+            }, 1000)
+            if (count2==5){
+                val intent = Intent(this, AdminActivity::class.java)
+                startActivity(intent)
+                overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_up)
+                count2 = 0
+            }
         }
-        //save value of sum
-        //show/hidden edit text
     }
 
    private fun generateArray(): String{
@@ -184,6 +218,55 @@ class MainActivity : AppCompatActivity() {
         }
         //1BT
         if(degrees > 324 && degrees <=360){
+            text = "1 bịch bánh tráng"
+            imGift.setImageResource(R.drawable.banh_trang_1)
+            Log.d("uytai", "1BT")
+        }
+        return text
+    }
+
+    //
+    fun getGift2(degrees: Float) : String{
+        Log.d("uytai", degrees.toString())
+        var text = ""
+        //no gift
+        if(degrees==0f || degrees==72f || degrees==144f || degrees==216f || degrees==360f){
+            text = "Trật rồi! Tiếc quá!"
+            imGift.setImageResource(R.drawable.mm)
+            Log.d("uytai", "MM")
+        }
+        //card 50
+        if(degrees == 36f){
+            text = "WAOO!! Thẻ cào 50K"
+            imGift.setImageResource(R.drawable.the_50k)
+            Log.d("uytai", "50")
+        }
+        //card 20
+        if(degrees == 108f){
+            text = "Thẻ cào 20K"
+            imGift.setImageResource(R.drawable.the_20k)
+            Log.d("uytai", "20")
+        }
+        //card 10
+        if(degrees == 180f){
+            text = "Thẻ cào 10K"
+            imGift.setImageResource(R.drawable.the_10k)
+            Log.d("uytai", "10")
+        }
+        //strongbow
+        if(degrees == 252f){
+            text = "1 ly Strongbow nhé!"
+            imGift.setImageResource(R.drawable.strongbow)
+            Log.d("uytai", "SB")
+        }
+        //2BT
+        if(degrees == 288f){
+            text = "2 bịch bánh tráng luôn!"
+            imGift.setImageResource(R.drawable.banh_trang_2)
+            Log.d("uytai", "2BT")
+        }
+        //1BT
+        if(degrees == 324f){
             text = "1 bịch bánh tráng"
             imGift.setImageResource(R.drawable.banh_trang_1)
             Log.d("uytai", "1BT")
